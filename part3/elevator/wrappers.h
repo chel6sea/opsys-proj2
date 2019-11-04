09f72588f64a6b30283f5e288626b42baf2122c6
osboxes.org <cj_gary@mac.com>
2019-11-03 19:34:59 -0500

    fixed wrappers.h

--- part3/elevator/wrappers.h
+++ part3/elevator/wrappers.h
@@ -10,15 +10,15 @@
 #define __NR_STOP_ELEVATOR 337
 
 int start_elevator() {
-	return syscall(335);
+	return syscall(__NR_START_ELEVATOR);
 }
 
 int issue_request(int type, int start, int dest) {
-	return syscall(336, type, start, dest);
+	return syscall(__NR_ISSUE_REQUEST, type, start, dest);
 }
 
 int stop_elevator() {
-	return syscall(337);
+	return syscall(__NR_STOP_ELEVATOR);
 }
 
 #endif
